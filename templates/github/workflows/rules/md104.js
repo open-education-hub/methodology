f0986740f0769d60039c988dbd4fbe38e7f5b2f5
David Bors <daviddvd267@gmail.com>
2022-09-28 17:16:36 +0300

    Add Super-Linter Github Actions configuration files
    Add configuration files needed in order to integrate Super-Linter to any
    Github repository.
    
    Signed-off-by: David Bors <daviddvd267@gmail.com>

---
+++ templates/github/workflows/rules/md104.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  names: ["MD104", "one line per sentence"],
+  description: "one line (and only one line) per sentence",
+  tags: ["sentences"],
+  function: function rule(params, onError) {
+    for (const inline of params.tokens.filter(function filterToken(token) {
+      return token.type === "inline";
+    })) {
+      var actual_lines = inline.content.split("\n");
+      actual_lines.forEach((line, index, arr) => {
+		let outside = true;
+		let outside_ticks = true;
+		let count = 0;
+		Array.from(line).forEach((char) => {
+			if ((char == "." || char == "?" || char == "!" || char == ";" || char == ":") && 
+				(outside && outside_ticks)) {
+				count++;
+			}
+			if (char == "`") outside_ticks = !outside_ticks;
+			if (char == "[") outside = false;
+			if (char == "(") outside = false;
+			if (char == "]") outside = true;
+			if (char == ")") outside = true;
+		});
+        if (count > 1) {
+          onError({
+            lineNumber: inline.lineNumber + index,
+            detail:
+              "Expected one sentence per line. Multiple end of sentence punctuation signs found on one line!",
+          });
+        }
+      });
+    }
+  },
+};
